0e9251f8ce01361cde02aad8ae0a42df94a00483
Obodo Bright <obodobright0@gmail.com>
2024-09-02 21:38:52 +0100

    fix women and men categories

--- src/pages/women/index.js
+++ src/pages/women/index.js
@@ -1,15 +1,38 @@
 import { Inter } from "next/font/google";
 import { CustomLayout } from "@/components/layout";
 import { CustomWrapper } from "@/components/layout/customWrapper";
+import { Breadcrumb } from "@/components/breadcrumb";
+import { ComponentTitle } from "@/components/typography/ComponentTitle";
+import { CategoryCard } from "@/components/cards/categoryCard";
+import { Footer } from "@/components/ui/footer";
+import { newArivalData } from "@/utils/data";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Women() {
+export default function MenComponent() {
   return (
     <main>
       <CustomLayout>
         <main>
-          <CustomWrapper>Women</CustomWrapper>
+          <CustomWrapper>
+            <Breadcrumb url="Women" />
+            <section className="my-3 flex items-center justify-between">
+              <ComponentTitle title="Women" />
+              <div className="">
+                <p className="text-base text-[#00000099]">
+                  {" "}
+                  Showing 1-10 of 100 Products Sort by:{" "}
+                  <button className=" text-black font-medium">Newest In</button>
+                </p>
+              </div>
+            </section>
+            <section className="flex flex-wrap gap-12 mt-10 mb-20 justify-center grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] items-center  ">
+              {newArivalData.map((cat, catIndex) => (
+                <CategoryCard data={cat} key={catIndex} />
+              ))}
+            </section>
+          </CustomWrapper>
+          <Footer />
         </main>
       </CustomLayout>
     </main>
