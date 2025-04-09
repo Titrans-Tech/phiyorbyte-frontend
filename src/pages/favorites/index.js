4fb692916d353ba645eba36e2b6e725983506b7b
Obodo Bright <obodobright0@gmail.com>
2025-04-09 17:03:30 +0100

    old updates.

--- src/pages/favorites/index.js
+++ src/pages/favorites/index.js
@@ -6,6 +6,7 @@ import { CustomWrapper } from "@/components/layout/customWrapper";
 import { ComponentTitle } from "@/components/typography/ComponentTitle";
 import { Footer } from "@/components/ui/footer";
 import { useGetFavourite } from "@/hooks/useCategoryApi";
+import { WithCommas } from "@/utils";
 import { favData } from "@/utils/data";
 import Image from "next/image";
 import Link from "next/link";
@@ -29,27 +30,30 @@ const Favourites = () => {
                 <>
                   {favourites?.data?.length > 0 ? (
                     <div className="border space-y-3 py-3 px-3 my-5 max-w-lg rounded-md ">
-                      {favData.map((fav, favIndex) => (
-                        <div
+                      {favourites?.data?.map((fav, favIndex) => (
+                        <Link
+                          href={`/product/${fav.ref_no}`}
                           key={favIndex}
-                          className="border-b py-1 flex items-center justify-between"
+                          className="border-b py-1 "
                         >
-                          <div className="flex items-start gap-2">
-                            <Image
-                              src={`/assets/category/${fav.img}`}
-                              width={70}
-                              height={70}
-                              alt="category-img"
-                            />
-                            <div>
-                              <h3 className="font-bold text-base">{fav.name}</h3>
-                              <p className="text-sm font-normal">Size: Large</p>
-                              <p className="text-sm font-normal">Color: {fav.color} </p>
-                              <h2 className="font-bold text-lg">{fav.price}</h2>
+                          <div className="flex items-center justify-between">
+                            <div className="flex items-start gap-2">
+                              <Image
+                                src={fav?.images1[0]}
+                                width={70}
+                                height={70}
+                                alt="category-img"
+                              />
+                              <div>
+                                <h3 className="font-bold text-base">{fav.brand_name}</h3>
+                                <p className="text-sm font-normal">Size: {fav.product_size}</p>
+                                <p className="text-sm font-normal">Color: {fav.product_colors} </p>
+                                <h2 className="font-bold text-lg">{WithCommas(fav.amount)}</h2>
+                              </div>
                             </div>
+                            <RiDeleteBin6Fill color="red" cursor="pointer" fontSize={20} />
                           </div>
-                          <RiDeleteBin6Fill color="red" cursor="pointer" fontSize={20} />
-                        </div>
+                        </Link>
                       ))}
                     </div>
                   ) : (
